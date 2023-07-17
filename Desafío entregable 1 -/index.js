08c3b90b984480e754719197e6b4a8f685ecd82a
Orianadt <118787469+Orianadt@users.noreply.github.com>
2023-07-17 20:41:35 -0300

    Add files via upload

---
+++ Desafío entregable 1 -/index.js
@@ -0,0 +1,27 @@
+class productManager{
+    constructor (products = []){
+    this.title = nombreProducto,
+    this.description = descripcionProducto,
+    this.price = precio,
+    this.thumbnail = rutaImagen,
+    this.code = codigoIdentificador,
+    this.stock = piezasDisponible
+}  
+}
+
+addProduct (nuevoProducto); {
+    products.push(nuevoProducto)
+let id= parseInt(document.getElementById('id_subcat').value);
+function x(){
+    id++;
+    return id;
+    }
+}
+
+getProducts(); {
+    return products.lenght;
+}
+
+getProductById(); {
+let products = document.getProductById ("products");
+}
